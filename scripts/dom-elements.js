1d4c9c3ca6e0e0a4f6c01df9fe2c8bd88d523ceb
Gustagols <maltag.tavow@gmail.com>
2025-09-25 23:10:31 -0300

    add: sync with main

--- scripts/dom-elements.js
+++ scripts/dom-elements.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export const emojiElement = document.querySelector('.js-emoji');
 export const scoreElement = document.querySelector('.js-score');
 export const questionBtn = document.querySelector('.question-button');
@@ -8,14 +7,3 @@ export const paperBtn = document.querySelector('.paper-button');
 export const scissorsBtn = document.querySelector('.scissors-button');
 export const resetBtn = document.querySelector('.reset-score');
 export const autoplayBtn = document.querySelector('.auto-play');
-=======
-export const emojiElement = document.querySelector('.js-emoji');
-export const scoreElement = document.querySelector('.js-score');
-export const questionBtn = document.querySelector('.question-button');
-export const trophyBtn = document.querySelector('.trophy-button');
-export const rockBtn = document.querySelector('.rock-button');
-export const paperBtn = document.querySelector('.paper-button');
-export const scissorsBtn = document.querySelector('.scissors-button');
-export const resetBtn = document.querySelector('.reset-score');
-export const autoplayBtn = document.querySelector('.auto-play');
->>>>>>> ce214bb86034bb54883c37aa4436ad44b87ee789
